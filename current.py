22d27faec8b81b66c9cd68fe0bc885d3d16726a8
Danylo Krysevych <dnl-krsvch@Danylos-MacBook-Pro.local>
2019-07-03 11:44:02 +0300

    Add get_period function

---
+++ current.py
@@ -0,0 +1,43 @@
+import requests
+import pytemperature
+import time
+import datetime
+
+
+
+def parse(url, *args):
+    request = requests.get(url.format(*args)).json()
+    return request
+
+
+def convert_time(s):
+    now = time.localtime(s)
+    return str(now.tm_hour) + ':' + str(now.tm_min)
+
+
+def get_info(request):
+    city = request['name']
+    temp = int(pytemperature.f2c(request['main']['temp']))
+    desc = request['weather'][0]['description']
+    humdity = request['main']['humidity']
+    sunrise = convert_time(request['sys']['sunrise'])
+    sunset = convert_time(request['sys']['sunset'])
+
+    weather = {
+        'Город:': city,
+        'Дата:': datetime.datetime.today().strftime('%d.%m.%Y'),
+        'Температура:': str(temp) + '°C',
+        'Описание:': desc,
+        'Влажность:': str(humdity) + '%',
+        'Восход:': sunrise,
+        'Закат:': sunset,
+    }
+    return weather
+
+
+if __name__ == "__main__":
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+    city = input('Введите город: ')
+    weather = get_info(parse(url, city))
+    for key, value in weather.items():
+    	print(key, value)
---
+++ get_period.py
@@ -0,0 +1,45 @@
+import requests
+import current
+import pytemperature
+import time
+
+
+def get_info(request):
+    data = []
+    city = request['city']['name']
+
+    for day in request['list']:
+        convert = time.localtime(day['dt'])
+        date = '0' + str(convert.tm_mday) + '.' + '0' + str(convert.tm_mon)
+        desc = day['weather'][0]['description']
+        temp_day = int(pytemperature.k2c(day['temp']['day']))
+        temp_night = int(pytemperature.k2c(day['temp']['night']))
+        humidity = str(day['humidity']) + '%'
+
+        data.append({
+            'Город:': city,
+            'Дата:': date,
+            'Температура:': {'в день:': str(temp_day) + '°C', 'в ноч:': str(temp_night) + '°C'},
+            'Влажность:': humidity,
+            'Описание:': str(desc),
+        })
+    return data
+
+
+if __name__ == "__main__":
+    url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q={}&cnt={}&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+    city = input('Введите город: ')
+    days = input('Введите количество дней: ')
+    request = current.parse(url, city, days)
+    data = get_info(request)
+    for_city = '------------------------------------------- ' + \
+        city + ' ----------------------------------------'
+    print(for_city)
+    for day in data:
+        date = day['Дата:']
+        for_day = '------------------------------------------ ' + \
+            date + ' ----------------------------------------'
+        print(for_day)
+        print('Температура: ' + 'в день: ' + day['Температура:']['в день:'] + ' в ноч: ' + day['Температура:']['в ноч:'])
+        print('Влажность:', day['Влажность:'])
+        print('Описание:', day['Описание:'])
